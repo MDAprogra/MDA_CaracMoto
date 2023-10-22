73674875cf66d0d04f45fd41b484ff18cbcba85d
MDAprogra <matth4627@gmail.com>
2023-10-22 22:05:32 +0200

    Ajout de la fonctionnalité FilePicker

--- MDA_CaracMoto/Pages/AjouterMoto.xaml.cs
+++ MDA_CaracMoto/Pages/AjouterMoto.xaml.cs
@@ -7,6 +7,7 @@ namespace MDA_CaracMoto.Pages;
 public partial class AjouterMoto : ContentPage
 {
     public string Extension { get; set; }
+    public string AudioMoto { get; set; }
 
     private readonly IDataService _dataService;
     public AjouterMoto()
@@ -20,12 +21,12 @@ public partial class AjouterMoto : ContentPage
     }
     async void Btn_Valider(object sender, EventArgs e)
     {
-        if (Marque.Text == null || Modele.Text == null || Reference.Text == null)
+        if (Marque.Text == null || Modele.Text == null || Reference.Text == null || Image.Text == null || ChoixExt.SelectedItem.ToString() == null)
         {
             await DisplayAlert("Erreur", "Veuillez remplir tous les champs", "OK");
             return;
         }
-        Extension=ChoixExt.SelectedItem.ToString();
+        Extension = ChoixExt.SelectedItem.ToString();
         CaracMoto moto = new CaracMoto
         {
             Ref = Reference.Text,
@@ -37,7 +38,8 @@ public partial class AjouterMoto : ContentPage
             KW = Convert.ToInt32(KW.Text),
             Poids = Convert.ToInt32(Poids.Text),
             Img = Image.Text.Trim() + Extension,
-            Audio = Audio.Text.Trim()
+            //Audio = Audio.Text.Trim()
+            Audio = AudioMoto
         };
         _dataService.AddMoto(moto);
         await DisplayAlert("Ajout", "Moto ajoutée", "OK");
@@ -53,5 +55,28 @@ public partial class AjouterMoto : ContentPage
         ChoixExt.SelectedItem = null;
         Image.Text = "";
         Audio.Text = "";
+    }
+
+    private async void AjoutAudio_Clicked(object sender, EventArgs e)
+    {
+        var motosound = await FilePicker.PickAsync(new PickOptions
+        {
+            //PickerTitle = "Sélectionner un fichier audio",
+            //FileTypes = FilePickerFileType.Videos
+            PickerTitle = "Sélectionner un fichier audio",
+            FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+            {
+                { DevicePlatform.WinUI, new[] { ".mp3", ".wav" } }
+            })
+        });
+        AudioMoto = motosound.FileName;
+        if (AudioMoto == null)
+            return;
+        else
+        {
+            AjoutAudio.Text = AudioMoto;
+            //Ajout de l'audio dans le fichier Raw
+
+        }
     }
 }
