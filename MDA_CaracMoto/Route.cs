fdc0a5e31a5f913fb2062ced443dceb0aaea9362
MDAprogra <matth4627@gmail.com>
2023-10-26 16:09:04 +0200

    Ajout de toutes les class "ViewModel" + class "Route"

---
+++ MDA_CaracMoto/Route.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDA_CaracMoto
+{
+    public static class Routes
+    {
+        public static class AudioRecorder
+        {
+            public const string RouteName = "audio-recorder";
+        }
+
+        public static class MusicPlayer
+        {
+            public const string RouteName = "music-player";
+
+            public static class Arguments
+            {
+                public const string Music = "music";
+            }
+        }
+    }
+}
---
+++ MDA_CaracMoto/ViewModels/MusicItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDA_CaracMoto.ViewModels
+{
+    public class MusicItemViewModel : BaseViewModel
+    {
+        public MusicItemViewModel(string title, string artist, string filename)
+        {
+            Title = title;
+            Artist = artist;
+            Filename = filename;
+        }
+
+        public string Title { get; }
+        public string Artist { get; }
+        public string Filename { get; }
+    }
+}
